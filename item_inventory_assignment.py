82ebc6dbdcab9910cef143d93684f8f5f3f6217f
Shreyas Patil <pshreyasv100@gmail.com>
2022-11-09 13:57:11 +0100

    updated stuff

--- item_inventory_assignment.py
+++ item_inventory_assignment.py
@@ -2,9 +2,9 @@ import numpy as np
 import random
 
 T = 1000
-x_max = 10
+x_max = 100
 # P(D=1) distribution
-Pd = [i/1000 for i in range(T + 1)]
+# Pd = [i/1000 for i in range(T + 1)]
 # holding cost
 h = 0.01
 # purchase cost
@@ -14,14 +14,14 @@ p = 20
 # order cost
 K = 10
 
-V = np.array([[float(0) for i in range(T + 1)] for j in range(x_max + 1)])
-alpha = np.array([[float(0)for i in range(T + 1)] for j in range(x_max + 1)])
+V = np.array([[float(0) for i in range(T)] for j in range(x_max + 1)])
+alpha = np.array([[float(0)for i in range(T)] for j in range(x_max + 1)])
 
 # initialized the last column: hx
 V[:, -1] = [x * h for x in range(0, x_max + 1)] 
 
 
-for t in range(T - 1, 0, -1):
+for t in range(T - 2, -1, -1):
     if t > 900:
         c = 0
     elif t > 500 and t <= 900:
@@ -29,15 +29,14 @@ for t in range(T - 1, 0, -1):
     else: 
         c = 10
 
-    dIsOne = Pd[t]
-    dIsZero = round(1 - dIsOne, 3)
+    dIsOne = t / 1000
+    dIsZero = 1 - dIsOne
     # P(d=1) increases linearly as t increases
     d = random.choices([1, 0], weights=(dIsOne, dIsZero), k = 1)[0]
     
     if t > 900:
         # At t > 900 no order can be placed, so the optimal policy would be just a copy of optimal policy at t = 900
-        for x in range(0, x_max):
-            Q = {}
+        for x in range(0, x_max+1):
             E = dIsOne * V[x - 1][t + 1] + dIsZero * V[x - 0][t + 1]  
             
             # items can only be sold
@@ -52,8 +51,8 @@ for t in range(T - 1, 0, -1):
             Q = {}
             for a in range(max(d-x,0) , x_max - x + d):
                 E = dIsOne * V[x - 1 + a][t + 1] + dIsZero * V[x - 0 + a][t + 1]
-                order_cost = K if a > 0 else 0    
-                Q[a] = (-h * x) - order_cost - (c * a)  + dIsOne * p + E
+                    
+                Q[a] = (-h * x) - K * (a > 1) - (c * a)  + dIsOne * p + E
             V[x][t] = Q[max(d-x, 0)]
             alpha[x][t]= max(d-x, 0)
             for a in range(max(d-x,0), x_max-x+d):
@@ -64,7 +63,7 @@ for t in range(T - 1, 0, -1):
 
 
 for i in range(x_max + 1):
-    for j in range(990, T + 1):
+    for j in range(990, T):
         print(V[i][j],end=' ')
     print() 
 print ('-------------------')
@@ -74,7 +73,7 @@ print ('-------------------')
 print('------------')
 
 for i in range(x_max + 1):
-    for j in range(990, T + 1):
+    for j in range(990, T):
         print(alpha[i][j], end = ' ')
     print()
 
